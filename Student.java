2660c5905ca70f87041f77c76e946dfa8492365e
Sebastien Campa <Sebastien.Campa@iCloud.com>
2022-10-03 08:53:23 -0500

    duh

--- Student.java
+++ Student.java
@@ -27,12 +27,23 @@ public class Student implements Serializable {
     }
 
     public double calculateGPA() {
+        double GPA = 0;
         for (int i = 0 ; i < classes.size(); i++) {
             int grade = classes.get(i).getGrade();
             if (grade > 89) {
-                
+                GPA += 4;
+            }
+            else if (grade > 79) {
+                GPA += 3;
+            }
+            else if (grade > 69) {
+                GPA += 2;
+            }
+            else if (grade > 59) {
+                GPA += 1;
             }
         }
+        return GPA / classes.size();
     }
 
     public Course getBestClass() {
